3def4b1f32a37762fafcec8be12663e128638fad
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-01-16 01:09:15 +0530

    Update start.py

--- AYESHA/utils/inline/start.py
+++ AYESHA/utils/inline/start.py
@@ -20,17 +20,18 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["S_B_14"],
+                url=f"https://youtube.com/@tseries?si=y7I_dU7yjiBbjG3V",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        [InlineKeyboardButton(text=_["S_B_12"], callback_data=f"abot_cb"),
+            InlineKeyboardButton(text=_["S_B_13"], callback_data=f"ubot_cb"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(
+                text=_["S_B_10"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
         ],
     ]
     return buttons
